daaa8c33167499617fcec35cf62f716626ba7bc3
sbrodovke <kostyasbrodov@gmail.com>
2020-03-18 19:30:35 +0500

    LightWeight

---
+++ LightWeight/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LightWeight
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var treeTypeFactory = new TreeTypeFactory();
+            var forest = new Forest(treeTypeFactory);
+
+            forest.Grow(new Point(1, 1), "bereza");
+            forest.Grow(new Point(4, 6), "bereza");
+
+            forest.Plant(new Point(4, 2), new Tree(new TreeType("dub", new Texture("dub")), 1, 2.5));
+
+            Console.WriteLine(forest.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LightWeight/Texture.cs
@@ -0,0 +1,17 @@
+﻿namespace LightWeight
+{
+    public class Texture
+    {
+        public Texture(string name)
+        {
+            Name = $"texture-{name}";
+        }
+
+        public string Name { get; }
+
+        public override string ToString()
+        {
+            return $"Name = {Name}";
+        }
+    }
+}
